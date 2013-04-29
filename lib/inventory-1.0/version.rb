80d87fa51d6d7aad7042c201aa63c4d89d156573
Nikolai Weibull <now@bitwi.se>
2013-04-29 22:35:35 +0200

    Update dependencies

--- lib/inventory-1.0/version.rb
+++ lib/inventory-1.0/version.rb
@@ -4,10 +4,10 @@ class Inventory
   Version = Inventory.new(1, 4, 0){
     def dependencies
       Dependencies.new{
-        development 'inventory-rake', 1, 3, 0
+        development 'inventory-rake', 1, 4, 0
         development 'lookout', 3, 0, 0
         development 'lookout-rake', 3, 0, 0
-        development 'yard', 0, 8, 0
+        development 'yard', 0, 8, 5
       }
     end
 
